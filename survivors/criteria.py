5d530520f2ae66928fc4f30a6abea7fe396ccd05
vasiliev <yuliivasiliev@gmail.com>
2022-08-11 17:50:40 +0300

    1. Embedded Numba Refactoring version of log-rank. It was inserted in criteria.py 2. Created ahead of time compilation version of log-rank, but it will be embedded too late (because, it needs to update lib architecture). 3. Soon, i try to prepare cython version.

--- survivors/criteria.py
+++ survivors/criteria.py
@@ -94,7 +94,7 @@ def cox(durations_A, durations_B, event_observed_A=None, event_observed_B=None) 
 ### 06/02/2022 Numpy+Numba 
 ### accelerate 2.8 according to library criteria
 @njit
-def coeffs_t_j(dur_A, dur_B, cens_A, cens_B, t_j, weightings):
+def iterate_coeffs_t_j(dur_A, dur_B, cens_A, cens_B, t_j, weightings):
     N_1_j = (dur_A >= t_j).sum()
     N_2_j = (dur_B >= t_j).sum()
     if N_1_j == 0 or N_2_j == 0:
@@ -118,7 +118,7 @@ def coeffs_t_j(dur_A, dur_B, cens_A, cens_B, t_j, weightings):
     return w_j, num, denom
 
 @njit
-def lr_statistic(dur_A, dur_B, cens_A, cens_B, times, weightings):
+def iterate_lr_statistic(dur_A, dur_B, cens_A, cens_B, times, weightings):
     res = np.zeros((times.shape[0], 3), dtype=np.float32)
     for j, t_j in enumerate(times):
         res[j] = coeffs_t_j(dur_A, dur_B, cens_A, cens_B, t_j, weightings)
@@ -130,21 +130,82 @@ def lr_statistic(dur_A, dur_B, cens_A, cens_B, times, weightings):
     return logrank
 
 
-def weight_lr_fast(dur_A, dur_B, cens_A = None, cens_B = None, weightings = ""):
+def iterate_weight_lr_fast(dur_A, dur_B, cens_A = None, cens_B = None, weightings = ""):
+    try:
+        if cens_A is None:
+            cens_A = np.ones(dur_A.shape[0])
+        if cens_B is None:
+            cens_B = np.ones(dur_B.shape[0])
+
+        #     a1 = np.unique(dur_A)
+        #     a2 = np.unique(dur_B)
+        #     times = np.unique(np.clip(np.union1d(a1,a2), 0, np.min([a1.max(), a2.max()])))
+        times = np.union1d(np.unique(dur_A), np.unique(dur_B))
+        logrank = lr_statistic(dur_A, dur_B, cens_A, cens_B, times, weightings)
+        pvalue = stats.chi2.sf(logrank, df=1)
+        return pvalue
+    except:
+        return 1.0
+
+
+# 11/08/2022 Numpy+Numba
+# dur_A = np.random.choice(10000, 10000)
+# cens_A = np.random.choice(2, 10000)
+# dur_B = np.random.choice(10000, 10000)
+# cens_B = np.random.choice(2, 10000)
+# new version (for 10000 elem): 2.16 ms ± 15.2 µs per loop
+# old version (for 10000 elem): 103 ms ± 171 µs per loop
+
+
+# @cuda.jit
+@njit('f8(i8[:], i8[:], i8[:], i8[:], i8[:], i8)', cache=True)
+def lr_statistic(dur_1, dur_2, cens_1, cens_2, times_range, weightings):
+    bins = times_range[1] - times_range[0] + 1
+    n_1_j = np.histogram(dur_1, bins=bins, range=times_range)[0]
+    n_2_j = np.histogram(dur_2, bins=bins, range=times_range)[0]
+    O_1_j = np.histogram(dur_1 * cens_1, bins=bins, range=times_range)[0]
+    O_2_j = np.histogram(dur_2 * cens_2, bins=bins, range=times_range)[0]
+
+    N_1_j = np.cumsum(n_1_j[::-1])[::-1]
+    N_2_j = np.cumsum(n_2_j[::-1])[::-1]
+    ind = np.where(N_1_j * N_2_j != 0)
+    N_1_j = N_1_j[ind]
+    N_2_j = N_2_j[ind]
+    O_1_j = O_1_j[ind]
+    O_2_j = O_2_j[ind]
+
+    N_j = N_1_j + N_2_j
+    O_j = O_1_j + O_2_j
+    E_1_j = N_1_j * O_j / N_j
+    res = np.zeros((N_j.shape[0], 3), dtype=np.float32)
+    res[:, 1] = O_1_j - E_1_j
+    res[:, 2] = E_1_j * (N_j - O_j) * N_2_j / (N_j * (N_j - 1))
+    res[:, 0] = 1.0
+    if weightings == 2:
+        res[:, 0] = N_j
+    elif weightings == 3:
+        res[:, 0] = np.sqrt(N_j)
+    elif weightings == 4:
+        res[:, 0] = np.cumprod((1.0 - O_j / (N_j + 1)))
+    logrank = np.power((res[:, 0] * res[:, 1]).sum(), 2) / ((res[:, 0] * res[:, 0] * res[:, 2]).sum())
+    return logrank
+
+
+def weight_lr_fast(dur_A, dur_B, cens_A=None, cens_B=None, weightings=""):
     """
     Count weighted log-rank criteria
 
     Parameters
     ----------
-    dur_A : array-like 
+    dur_A : array-like
         Time of occured events from first sample.
-    dur_B : array-like 
+    dur_B : array-like
         Time of occured events from second sample.
     cens_A : array-like, optional
-        Indicate of occured events from first sample. 
+        Indicate of occured events from first sample.
         The default is None (all events occured).
     cens_B : array-like, optional
-        Indicate of occured events from second sample. 
+        Indicate of occured events from second sample.
         The default is None (all events occured).
     weightings : str, optional
         Weights of criteria. The default is "" (log-rank).
@@ -160,16 +221,22 @@ def weight_lr_fast(dur_A, dur_B, cens_A = None, cens_B = None, weightings = ""):
 
     """
     try:
+        times = np.unique(np.hstack((dur_A, dur_B)))
+        dur_A = np.searchsorted(times, dur_A) + 1
+        dur_B = np.searchsorted(times, dur_B) + 1
+        times_range = np.array([1, times.shape[0]])
         if cens_A is None:
             cens_A = np.ones(dur_A.shape[0])
         if cens_B is None:
             cens_B = np.ones(dur_B.shape[0])
-
-        #     a1 = np.unique(dur_A)
-        #     a2 = np.unique(dur_B)
-        #     times = np.unique(np.clip(np.union1d(a1,a2), 0, np.min([a1.max(), a2.max()])))
-        times = np.union1d(np.unique(dur_A), np.unique(dur_B))
-        logrank = lr_statistic(dur_A, dur_B, cens_A, cens_B, times, weightings)
+        d = {"logrank": 1, "wilcoxon": 2, "tarone-ware": 3, "peto": 4}
+        weightings = d.get(weightings, 1)
+        logrank = lr_statistic(dur_A.astype("int64"),
+                               dur_B.astype("int64"),
+                               cens_A.astype("int64"),
+                               cens_B.astype("int64"),
+                               times_range.astype("int64"),
+                               np.int64(weightings))
         pvalue = stats.chi2.sf(logrank, df=1)
         return pvalue
     except:
