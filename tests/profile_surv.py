dbda63db61415eea0d7527462e3118f59057ad0e
vasiliev <yuliivasiliev@gmail.com>
2022-10-19 23:33:53 +0300

    1. Add profiler reports for pbc and onc dataset (1, 2, 4, 8, 16 process and vectorize) 2. Add ONK dataset to profiler script

--- tests/profile_surv.py
+++ tests/profile_surv.py
@@ -1,7 +1,7 @@
 import cProfile
 import pstats
 
-from survivors.datasets import load_pbc_dataset
+from survivors.datasets import load_pbc_dataset, load_onk_dataset
 from survivors.experiments.grid import generate_sample
 
 from test_experiments import run
@@ -10,32 +10,32 @@ from survivors.tree import CRAID
 from survivors.ensemble import BoostingCRAID
 
 
-def pbs_samples():
-    X, y, features, categ, sch_nan = load_pbc_dataset()
+def get_samples():
+    X, y, features, categ, sch_nan = load_onk_dataset()
     a = generate_sample(X, y, 5)
     return next(a)
 
 
 def profile_tree():
     # params = {"criterion": "peto", "depth": 2, "min_samples_leaf": 30, "signif": 0.05, "n_jobs": 1}
-    params = {"criterion": "peto", "depth": 10, "min_samples_leaf": 1, "signif": 0.05, "n_jobs": 16}
+    params = {"criterion": "peto", "depth": 10, "min_samples_leaf": 1, "signif": 0.05, "n_jobs": 1}
 
-    X_train, y_train, X_test, y_test, bins = pbs_samples()
+    X_train, y_train, X_test, y_test, bins = get_samples()
     profiler = cProfile.Profile()
     profiler.enable()
-    for i in range(10):
+    for i in range(5):
         craid_tree = CRAID(**params)
         craid_tree.fit(X_train, y_train)
     profiler.disable()
     stats = pstats.Stats(profiler).sort_stats('cumtime')
     stats.print_stats()
-    stats.dump_stats('profile_reports/tree_output_16.pstats')
+    stats.dump_stats('profile_reports/tree_output_onk_vect.pstats')
 
 
 def profile_boost():
     params = {"criterion": "peto", "depth": 5, "min_samples_leaf": 30, "n_estimators": 3, "n_jobs": 1}
 
-    X_train, y_train, X_test, y_test, bins = pbs_samples()
+    X_train, y_train, X_test, y_test, bins = get_samples()
     profiler = cProfile.Profile()
     profiler.enable()
     craid_tree = BoostingCRAID(**params)
