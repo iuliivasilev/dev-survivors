c855b6c524a36e4f9cc535fe802717ee7514333b
vasiliev <yuliivasiliev@gmail.com>
2022-10-20 23:59:25 +0300

    1. Add profiler results for ONC dataset. 2. Add full cycle for experiment.

--- tests/profile_surv.py
+++ tests/profile_surv.py
@@ -6,6 +6,8 @@ from survivors.experiments.grid import generate_sample
 
 from test_experiments import run
 
+
+from survivors.constants import get_bins, TIME_NAME, CENS_NAME
 from survivors.tree import CRAID
 from survivors.ensemble import BoostingCRAID
 
@@ -18,18 +20,24 @@ def get_samples():
 
 def profile_tree():
     # params = {"criterion": "peto", "depth": 2, "min_samples_leaf": 30, "signif": 0.05, "n_jobs": 1}
-    params = {"criterion": "peto", "depth": 10, "min_samples_leaf": 1, "signif": 0.05, "n_jobs": 1}
+    params = {"criterion": "peto", "depth": 10, "min_samples_leaf": 1, "signif": 0.05, "n_jobs": 2}
 
     X_train, y_train, X_test, y_test, bins = get_samples()
+
+    bins = get_bins(time=y_train[TIME_NAME], cens=y_train[CENS_NAME])
     profiler = cProfile.Profile()
     profiler.enable()
     for i in range(5):
         craid_tree = CRAID(**params)
         craid_tree.fit(X_train, y_train)
+        craid_tree.predict_at_times(X_test, bins=bins, mode="surv")
+        craid_tree.predict(X_test, target=TIME_NAME)
+        craid_tree.predict_at_times(X_test, bins=bins, mode="hazard")
+
     profiler.disable()
     stats = pstats.Stats(profiler).sort_stats('cumtime')
     stats.print_stats()
-    stats.dump_stats('profile_reports/tree_output_onk_vect.pstats')
+    stats.dump_stats('profile_reports/ONK/tree_output_2.pstats')
 
 
 def profile_boost():
