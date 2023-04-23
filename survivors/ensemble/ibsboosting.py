56b2e8ada99c2dfd6b93e68773a8e1b3b71cebd1
vasiliev <yuliivasiliev@gmail.com>
2023-04-23 22:19:40 +0300

    1. Developed IBS model (reached best results for ONK Hold out with hyperparameters {'balance': None, "with_arc": False, 'aggreg_func': 'wei', 'all_weight': True, 'categ': categ, 'criterion': 'logrank', 'depth': 10, 'ens_metric_name': 'bic', 'leaf_model': 'base_fast','max_features': "sqrt", 'min_samples_leaf': 5, 'n_estimators': 100, 'n_jobs': 2, 'size_sample': 0.5}

---
+++ survivors/ensemble/ibsboosting.py
@@ -0,0 +1,47 @@
+from .boosting import BoostingCRAID
+from .. import metrics as metr
+import numpy as np
+
+
+class IBSBoostingCRAID(BoostingCRAID):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "IBSBoostingCRAID"
+
+    def count_model_weights(self, model, X_sub, y_sub):
+        if self.all_weight:
+            X_sub = self.X_train
+            y_sub = self.y_train
+
+        pred_sf = model.predict_at_times(X_sub, bins=self.bins, mode="surv")
+
+        wei = metr.ibs(self.y_train, y_sub, pred_sf, self.bins, axis=0)
+        betta = np.mean(wei)
+        return wei, betta
+
+    def update_weight(self, index, wei_i):
+        if self.all_weight:
+            self.weights = self.weights + wei_i
+        else:
+            self.weights[index] = (self.weights[index] + wei_i)
+        self.weights = (self.weights - self.weights.min())
+        # self.weights = (self.weights - self.weights.min()) / (self.weights.max() - self.weights.min())
+
+    def get_aggreg(self, x):
+        if self.aggreg_func == 'median':
+            return np.median(x, axis=0)
+        if self.aggreg_func == 'moda':
+            res = np.median(x > 0.5, axis=0)
+            return res
+        if self.aggreg_func == 'complex':
+            # a = np.max(x, axis=0)
+            b = np.min(x, axis=0)
+            res = np.mean(x, axis=0)
+            # res[a == 1] = 1
+            res[res < 0.01] = 0
+            return res
+        elif self.aggreg_func == 'wei':
+            inv_wei = 1 / np.array(self.bettas)
+            wei = inv_wei / sum(inv_wei)
+            return np.sum((x.T * wei).T, axis=0)
+        return np.mean(x, axis=0)
