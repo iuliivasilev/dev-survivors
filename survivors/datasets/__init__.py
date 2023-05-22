b3111ebcf254d5398f118815b752697a609c024e
vasiliev <yuliivasiliev@gmail.com>
2023-05-22 23:45:24 +0300

    1. Updated list of loading functions

--- survivors/datasets/__init__.py
+++ survivors/datasets/__init__.py
@@ -1,5 +1,13 @@
 from .other import load_gbsg_dataset
 from .other import load_pbc_dataset
 from .other import load_wuhan_dataset
+
+from .other import load_actg_dataset
+from .other import load_flchain_dataset
+from .other import load_smarto_dataset
+from .other import load_rott2_dataset
+from .other import load_support2_dataset
+from .other import load_Framingham_dataset
+
 from .covid import load_covid_dataset
 from .onk import load_onk_dataset
