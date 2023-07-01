b64ab69e2a22a174182fb091cdfe711164f4efd4
vasiliev <yuliivasiliev@gmail.com>
2023-07-01 23:17:22 +0300

    1. Added empirical boosting parameters

--- tests/PARAMS/SCHEME_PARAM.py
+++ tests/PARAMS/SCHEME_PARAM.py
@@ -25,7 +25,26 @@ BSTR_param_grid = {
     "n_jobs": [5]
 }
 
+BOOST_param_grid = {
+    "aggreg_func": ['wei', 'mean'],
+    "size_sample": [0.7, 1.0],  # 0.7
+    "n_estimators": [50],
+    "depth": [10],
+    "mode_wei": ['exp', 'square', "sigmoid", "softmax"],
+    "ens_metric_name": ["IBS"],  # ["bic", "roc", "ibs"], , "IBS_WW", "IBS_REMAIN"
+    "criterion": ["maxcombo", "peto", "tarone-ware", "wilcoxon", "logrank"],
+    "leaf_model": ["base_zero_after"],  # "base_zero_after", "base_fast"
+    "balance": [None],  #, "balance", "only_log_rank"],  # [None, "balance", "balance+correct", "only_log_rank"]
+
+    "min_samples_leaf": [0.05, 0.001],
+    "max_features": [0.3, "sqrt"],
+    "n_jobs": [5],
+
+    "all_weight": [False, True]
+}
+
 SCHEME_PARAMS = {
     "TREE": CRAID_param_grid,
-    "BSTR": BSTR_param_grid
+    "BSTR": BSTR_param_grid,
+    "BOOST": BOOST_param_grid
 }
