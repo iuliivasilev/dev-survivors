12e553638c8322d6361fe1b5958d642528f5c223
vasiliev <yuliivasiliev@gmail.com>
2023-10-08 23:44:06 +0300

    1. Updated experiments

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -237,16 +237,16 @@ def dir_path():
 # )
 
 @pytest.mark.parametrize(
-    "best_metric", ["likelihood", "conc", "IBS", "IBS_WW", "IBS_REMAIN"]  # ["likelihood", "conc", "IBS", "IBS_WW", "IBS_REMAIN"]
+    "best_metric", ["IBS_REMAIN"]  # ["likelihood", "conc", "IBS", "IBS_WW", "IBS_REMAIN"]
 )
 @pytest.mark.parametrize(
-    "dataset", ["GBSG", "PBC", "WUHAN", "rott2", "smarto", "support2", "flchain", "actg"]  # "GBSG",
+    "dataset",  ["GBSG", "WUHAN"]  # , "GBSG", "WUHAN", "PBC", "rott2", "actg", "smarto", "support2", "flchain"]
 )
 def test_dataset_exp(dir_path, dataset, best_metric, bins_sch="origin", mode="CV+SAMPLE"):
-    prefix = f"{best_metric}_PVAL_BOOST_{bins_sch}"  # "CLEVERBOOST"  # "scsurv"  # "bstr_full_WB"
+    prefix = f"{best_metric}_REG_CLEVERBOOST_ALL_BINS_BSTR_{bins_sch}"  # "CLEVERBOOST"  # "scsurv"  # "bstr_full_WB"
     # res_exp = run(dataset, with_self=[], with_external=True, mode=mode,
     #               dir_path=dir_path+"\\", bins_sch=bins_sch, best_metric=best_metric)  # Only scikit-survival
-    res_exp = run(dataset, with_self=["BOOST"], with_external=False, mode=mode,  # CLEVERBOOST
+    res_exp = run(dataset, with_self=["CLEVERBOOST"], with_external=False, mode=mode,  # CLEVERBOOST
                   dir_path=dir_path+"\\", bins_sch=bins_sch, best_metric=best_metric)  # ["TREE", "BSTR", "BOOST"]
 
     df_full = res_exp.get_result()
