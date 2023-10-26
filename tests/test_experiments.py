9b8f2df111343a03600f7a7a84d979949a61e3a1
vasiliev <yuliivasiliev@gmail.com>
2023-10-26 23:31:40 +0300

    1. Run experiments for constant distance filtering

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -243,7 +243,7 @@ def dir_path():
     "dataset",  ["GBSG", "WUHAN", "PBC", "rott2"]  # "GBSG", "WUHAN", "PBC", "rott2", "actg", "smarto", "support2", "flchain"
 )
 def test_dataset_exp(dir_path, dataset, best_metric, bins_sch="origin", mode="CV+SAMPLE"):
-    prefix = f"{best_metric}_FULL_TIME_DIP_EQ_REG_CLEVERBOOST_ALL_BINS_{bins_sch}"  # "scsurv", "bstr_full_WB"
+    prefix = f"{best_metric}_FULL_TIME_CNT_DIFF_EQ_REG_CLEVERBOOST_ALL_BINS_{bins_sch}"  # "scsurv", "bstr_full_WB"
     # res_exp = run(dataset, with_self=[], with_external=True, mode=mode,
     #               dir_path=dir_path+"\\", bins_sch=bins_sch, best_metric=best_metric)  # Only scikit-survival
     res_exp = run(dataset, with_self=["CLEVERBOOST"], with_external=False, mode=mode,
