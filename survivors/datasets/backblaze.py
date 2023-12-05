c8744d5eb8b7d33bb3afc7f0014b35cb7e5c03e9
vasiliev <yuliivasiliev@gmail.com>
2023-12-05 23:24:48 +0300

    Added new dataset (backblaze) preparation script

---
+++ survivors/datasets/backblaze.py
@@ -0,0 +1,57 @@
+import pandas as pd
+# from survivors import constants as cnt
+from ..constants import TIME_NAME, CENS_NAME, get_y
+from os.path import dirname, join
+
+### Функции скачивания датасета
+
+def str_to_categ(df_col):
+    uniq = df_col.unique()
+    return df_col.map(dict(zip(uniq, range(len(uniq)))))
+
+
+dir_env = join(dirname(__file__), "data", "BACKBLAZE")
+
+def load_smart_2017_date_not_all_year():
+    df = pd.read_csv(join(dir_env, 'smart_2017_date_not_all_year.csv'))
+    df = df.rename({"time": TIME_NAME, "event": CENS_NAME}, axis=1)
+    df = df.drop(columns=['serial_number', 'failure', 'date'])
+    categ = ['model']
+    df['model'] = str_to_categ(df['model'])
+    sign = list(set(df.columns) - set([CENS_NAME, TIME_NAME]))
+    y = get_y(df[CENS_NAME], df[TIME_NAME])
+    X = df.loc[:, sign].reset_index()
+    return X, y, sign, categ, []
+
+def load_backblaze_dataset():  # load_smart_2017_date_not_in_last_report():
+    df = pd.read_csv(join(dir_env, 'smart_2017_date_not_in_last_report.csv'))
+    df = df.rename({"time": TIME_NAME, "event": CENS_NAME}, axis=1)
+    df = df.drop(columns=['serial_number', 'failure', 'date'])
+    categ = ['model']
+    df['model'] = str_to_categ(df['model'])
+    sign = sorted(list(set(df.columns) - set([CENS_NAME, TIME_NAME])))
+    y = get_y(df[CENS_NAME], df[TIME_NAME])
+    X = df.loc[:, sign].reset_index()
+    return X, y, sign, categ, []
+
+def load_smart_2017_raw_9_not_all_year():
+    df = pd.read_csv(join(dir_env, 'smart_2017_raw_9_not_all_year.csv'))
+    df = df.rename({"time": TIME_NAME, "event": CENS_NAME}, axis=1)
+    df = df.drop(columns=['serial_number', 'failure', 'date'])
+    categ = ['model']
+    df['model'] = str_to_categ(df['model'])
+    sign = list(set(df.columns) - set([CENS_NAME, TIME_NAME]))
+    y = get_y(df[CENS_NAME], df[TIME_NAME])
+    X = df.loc[:, sign].reset_index()
+    return X, y, sign, categ, []
+
+def load_smart_2017_raw_9_not_in_last_report():
+    df = pd.read_csv(join(dir_env, 'smart_2017_raw_9_not_in_last_report.csv'))
+    df = df.rename({"time": TIME_NAME, "event": CENS_NAME}, axis=1)
+    df = df.drop(columns=['serial_number', 'failure', 'date'])
+    categ = ['model']
+    df['model'] = str_to_categ(df['model'])
+    sign = list(set(df.columns) - set([CENS_NAME, TIME_NAME]))
+    y = get_y(df[CENS_NAME], df[TIME_NAME])
+    X = df.loc[:, sign].reset_index()
+    return X, y, sign, categ, []
