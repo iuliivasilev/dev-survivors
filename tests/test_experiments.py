5e9fe75ea3cd4697c7674ea737b95497079b3783
vasiliev <yuliivasiliev@gmail.com>
2023-12-27 22:53:49 +0300

    Updated experiments

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -242,18 +242,18 @@ def dir_path():
     "best_metric", ["IBS_REMAIN"]  # ["likelihood", "conc", "IBS", "IBS_WW", "IBS_REMAIN"]
 )
 @pytest.mark.parametrize(
-    "dataset",  ["backblaze", "support2"]  # ["WUHAN", "PBC", "actg", "rott2", "GBSG", "support2", "flchain", "smarto"]
+    "dataset",  ["PBC", "GBSG", "WUHAN", "actg", "rott2", "support2", "flchain", "smarto"]  # "backblaze",
 )
-def test_dataset_exp(dir_path, dataset, best_metric, bins_sch="origin", mode="CV"):  # CV+SAMPLE
+def test_dataset_exp(dir_path, dataset, best_metric, bins_sch="origin", mode="CV+SAMPLE"):
     # NORMAL_SHORT_QUANTILE_TIME_
-    # prefix = f"{best_metric}_STRATTIME+_EXT10_NORMAL_EQ_REG_CLEVERBOOST+_ALL_BINS_{bins_sch}"  # "scsurv", "bstr_full_WB", SHORT_CNT_DIFF_
-    prefix = f"{best_metric}_scsurv_{bins_sch}"  # "scsurv", "bstr_full_WB", SHORT_CNT_DIFF_
+    prefix = f"{best_metric}_STRATTIME+_EXT10_NORMAL_EQ_REG_CLEVERBOOST_SUM_ALL_BINS_{bins_sch}"  # "scsurv", "bstr_full_WB", SHORT_CNT_DIFF_
+    # prefix = f"{best_metric}_scsurv_{bins_sch}"  # "scsurv", "bstr_full_WB", SHORT_CNT_DIFF_
     # res_exp = run(dataset, with_self=[], with_external=True, mode=mode,
     #               dir_path=dir_path+"\\", bins_sch=bins_sch, best_metric=best_metric)  # Only scikit-survival
     storage_path = os.path.join("D:", os.sep, "Vasilev", "SA", dataset)
     if not os.path.exists(storage_path):
         os.makedirs(storage_path)
-    res_exp = run(dataset, with_self=[], with_external=True, mode=mode,  # CLEVERBOOST
+    res_exp = run(dataset, with_self=["CLEVERBOOST"], with_external=False, mode=mode,  # CLEVERBOOST
                   dir_path=storage_path+"\\", bins_sch=bins_sch, best_metric=best_metric)  # ["TREE", "BSTR", "BOOST"]
 
     df_full = res_exp.get_result()
