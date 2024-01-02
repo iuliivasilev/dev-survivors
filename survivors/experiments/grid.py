7797864f305e84a3063baab9c3fc008fbc828b5e
vasiliev <yuliivasiliev@gmail.com>
2024-01-02 23:54:41 +0300

    Delete unbreakable closure

--- survivors/experiments/grid.py
+++ survivors/experiments/grid.py
@@ -302,27 +302,27 @@ class Experiments(object):
             grid_params = ParameterGrid(grid)
             p_size = len(grid_params)
             for i_p, p in enumerate(grid_params):
-                try:
-                    start_time = time.time()
-                    eval_metr = fit_eval_func(**p)
-                    full_time = time.time() - start_time
-                    curr_dict = {"METHOD": method.__name__, "CRIT": p.get("criterion", ""),
-                                 "PARAMS": str(p), "TIME": full_time}
-                    eval_metr = {m: eval_metr[:, i] for i, m in enumerate(self.metrics)}
-                    curr_dict.update(eval_metr)  # dict(zip(self.metrics, eval_metr))
-                    # self.result_table = self.result_table.append(curr_dict, ignore_index=True)
-                    self.result_table = pd.concat([self.result_table, pd.DataFrame([curr_dict])], ignore_index=True)
-                    if verbose > 0:
-                        print(f"Iteration: {i_p + 1}/{p_size}")
-                        print(f"EXECUTION TIME OF {method.__name__}: {full_time}",
-                                  {k: [np.mean(v[:-1]), v[-1]] for k, v in eval_metr.items()})  # np.mean(v)
-                except KeyboardInterrupt:
-                    print("HANDELED KeyboardInterrupt")
-                    break
-                except Exception as e:
-                    print("Method: %s, Param: %s finished with except '%s'" % (method.__name__, str(p), e))
-                    #if self.except_stop == "all":
-                    #    break
+                #try:
+                start_time = time.time()
+                eval_metr = fit_eval_func(**p)
+                full_time = time.time() - start_time
+                curr_dict = {"METHOD": method.__name__, "CRIT": p.get("criterion", ""),
+                             "PARAMS": str(p), "TIME": full_time}
+                eval_metr = {m: eval_metr[:, i] for i, m in enumerate(self.metrics)}
+                curr_dict.update(eval_metr)  # dict(zip(self.metrics, eval_metr))
+                # self.result_table = self.result_table.append(curr_dict, ignore_index=True)
+                self.result_table = pd.concat([self.result_table, pd.DataFrame([curr_dict])], ignore_index=True)
+                if verbose > 0:
+                    print(f"Iteration: {i_p + 1}/{p_size}")
+                    print(f"EXECUTION TIME OF {method.__name__}: {full_time}",
+                              {k: [np.mean(v[:-1]), v[-1]] for k, v in eval_metr.items()})  # np.mean(v)
+                # except KeyboardInterrupt:
+                #     print("HANDELED KeyboardInterrupt")
+                #     break
+                # except Exception as e:
+                #     print("Method: %s, Param: %s finished with except '%s'" % (method.__name__, str(p), e))
+                #     #if self.except_stop == "all":
+                #     #    break
         if self.mode in ["TIME-CV", "CV+HOLD-OUT"]:
             for m in self.metrics:
                 self.result_table[f"{m}_pred_mean"] = self.result_table[m].apply(lambda x: np.mean(x[:-1]))
