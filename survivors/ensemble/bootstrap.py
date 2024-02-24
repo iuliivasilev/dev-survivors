f29e510244299fc1104cc455a2ac97abd073dce6
vasiliev <yuliivasiliev@gmail.com>
2024-02-24 15:31:25 +0300

    1. Created ParallelBootstrap model

--- survivors/ensemble/bootstrap.py
+++ survivors/ensemble/bootstrap.py
@@ -3,6 +3,7 @@ import numpy as np
 from ..tree import CRAID
 from .. import constants as cnt
 from .base_ensemble import FastBaseEnsemble
+from joblib import Parallel, delayed
 
 
 class BootstrapCRAID(FastBaseEnsemble):
@@ -39,10 +40,70 @@ class BootstrapCRAID(FastBaseEnsemble):
             x_oob = self.X_train.loc[self.X_train.index.difference(x_sub.index), :]
 
             x_sub = x_sub.reset_index(drop=True)
-            X_sub_tr, y_sub_cr = cnt.pd_to_xy(x_sub)
+            X_sub_tr, y_sub_tr = cnt.pd_to_xy(x_sub)
 
             model = CRAID(features=self.features, random_state=i, **self.tree_kwargs)
-            model.fit(X_sub_tr, y_sub_cr)
+            model.fit(X_sub_tr, y_sub_tr)
 
             self.add_model(model, x_oob)
         print(f"fitted: {len(self.models)} models.")
+
+
+class ParallelBootstrapCRAID(BootstrapCRAID):
+
+    @staticmethod
+    def fit_tree(x_sub, params):
+        X_tr, y_tr = cnt.pd_to_xy(x_sub)
+        model = CRAID(**params)
+        model.fit(X_tr, y_tr)
+        return model
+
+    def fit(self, X, y):
+        self.features = X.columns
+        X = X.reset_index(drop=True)
+        X[cnt.CENS_NAME] = y[cnt.CENS_NAME].astype(np.int32)
+        X[cnt.TIME_NAME] = y[cnt.TIME_NAME].astype(np.float32)
+
+        self.X_train = X
+        self.y_train = y
+        self.update_params()
+
+        p_s = []
+        x_sub_ind = []
+        for i in range(self.n_estimators):
+            x_sub = self.X_train.sample(n=self.size_sample, replace=self.bootstrap, random_state=i)
+            x_sub_ind.append(x_sub.index)
+
+            x_sub = x_sub.reset_index(drop=True)
+            params = self.tree_kwargs.copy()
+            params['random_state'] = i
+            params['features'] = self.features
+            p_s.append({"x_sub": x_sub, "params": params})
+
+        with Parallel(n_jobs=self.tree_kwargs.get("n_jobs", 10), verbose=False, batch_size=10) as parallel:
+            ml = parallel(delayed(self.fit_tree)(**p) for p in p_s)
+
+        for model, x_sub_ind in zip(ml, x_sub_ind):
+            x_oob = self.X_train.loc[self.X_train.index.difference(x_sub_ind), :]
+            self.add_model(model, x_oob)
+        print(f"fitted: {len(self.models)} models.")
+
+    # def predict_at_times(self, x_test, bins, aggreg=True, mode="surv"):
+    #     with Parallel(n_jobs=self.tree_kwargs.get("n_jobs", 10), verbose=False, batch_size=10) as parallel:
+    #         ml = parallel(delayed(lambda m: m.predict_at_times(x_test, bins=bins,
+    #                                                            mode=mode)[np.newaxis, :])(m) for m in self.models)
+    #     res = np.vstack(ml)
+    #     if aggreg:
+    #         res = self.get_aggreg(res)
+    #         if mode == "surv":
+    #             res[:, -1] = 0
+    #             res[:, 0] = 1
+    #     return res
+    #
+    # def predict(self, x_test, aggreg=True, **kwargs):
+    #     with Parallel(n_jobs=self.tree_kwargs.get("n_jobs", 10), verbose=False, batch_size=10) as parallel:
+    #         ml = parallel(delayed(lambda m: m.predict(x_test, **kwargs))(m) for m in self.models)
+    #     res = np.array(ml)
+    #     if aggreg:
+    #         res = self.get_aggreg(res)
+    #     return res
