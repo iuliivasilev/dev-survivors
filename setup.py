a616ab8c640a643c0d7025b82f8b7af5ccc1aea5
vasiliev <yuliivasiliev@gmail.com>
2024-02-28 10:49:49 +0300

    1. Updated installation

--- setup.py
+++ setup.py
@@ -31,15 +31,33 @@ def parse_requirements(filename):
 # 3. (test) twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ dist/* -u **** -p ****
 # 4. (release) twine upload --skip-existing --repository-url https://upload.pypi.org/legacy/ dist/* -u **** -p ****
 
+# requirements_list = ["joblib >= 1.2.0",
+#     "pickle-mixin",
+#     "numpy >= 1.22",
+#     "numba >= 0.58.0",
+#     "matplotlib >= 3.5.0",
+#     "seaborn",
+#     "graphviz >= 0.20",
+#     "pandas >=0.25",
+#     "scipy >= 1.11.0",
+#     "scikit-learn >= 1.0.2",
+#     "lifelines >= 0.27.8",
+#     "scikit-survival >= 0.17.2",
+#     "openpyxl"]
+
+install_requires = parse_requirements("requirements/requirements.txt")
+
 setup(
     name='survivors',
     version='1.6.0',
+    license='BSD 3-Clause License',
     author='Iulii Vasilev',
     author_email='iuliivasilev@gmail.com',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
-    include_package_data=False,
+    include_package_data=True,
     package_data=PACKAGE_DATA,
-    python_requires='>=3.7',
-    install_requires=parse_requirements("./requirements.txt")  # requirements_list,
+    python_requires='>=3.10',
+    install_requires=install_requires,  # requirements_list
+    keywords=["survival analysis", "time-to-event", "event data", "machine learning"]
 )
