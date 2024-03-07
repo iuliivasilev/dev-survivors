720710e36838ee5867631da25e07e3e23cbe6d79
vasiliev <yuliivasiliev@gmail.com>
2024-03-07 23:18:28 +0300

    Updated experiments

--- tests/PARAMS/SCHEME_PARAM.py
+++ tests/PARAMS/SCHEME_PARAM.py
@@ -1,11 +1,11 @@
 CRAID_param_grid = {
-    "depth": [15],  # 10
+    "depth": [10],  # 10
     "balance": [None],  # [None, "balance", "balance+correct", "only_log_rank"]
     "criterion": ["peto", "tarone-ware", "wilcoxon", "logrank"],  # "ibswei", "maxcombo",
     "min_samples_leaf": [0.05, 0.01, 0.001],
     "l_reg": [0, 0.01, 0.1, 0.5, 0.9],  # [0, 0.001, 0.01, 0.1, 0.2]
     "leaf_model": ["base_zero_after"],
-    'cut': [True, False],
+    'cut': [False],  # True,
     "woe": [True],
     "signif": [0.05, 0.1, 1.0],
     "max_features": [1.0],
@@ -13,18 +13,37 @@ CRAID_param_grid = {
 }
 
 BSTR_param_grid = {
-    "size_sample": [0.7, 1.0],  # 0.7
+    "aggreg_func": ["mean"],
+    "size_sample": [0.3, 0.5, 0.7, 1.0],  # 0.7
     "n_estimators": [50],
-    "depth": [10],  # 15
-    "ens_metric_name": ["IBS"],  # ["bic", "roc", "ibs"], "IBS_WW", "IBS_REMAIN"
-    "criterion": ["maxcombo", "peto", "tarone-ware", "wilcoxon", "logrank"],  # "ibswei",
+    "depth": [7],  # 15
+    "l_reg": [0, 0.01, 0.1, 0.5, 0.9],
+    "ens_metric_name": ["IBS_REMAIN"],  # ["bic", "roc", "ibs"], "IBS_WW", "IBS_REMAIN"
+    "criterion": ["peto", "tarone-ware", "wilcoxon", "logrank"],  # "ibswei",
     "leaf_model": ["base_zero_after"],  # ["base_fast", "base_zero_after"]
     "balance": [None],  # [None, "balance", "balance+correct", "only_log_rank"]
 
     "min_samples_leaf": [0.05, 0.01, 0.001],
     "max_features": [0.3, "sqrt"],
-    "n_jobs": [5]
+    "n_jobs": [15]
 }
+
+PAR_BSTR_param_grid = BSTR_param_grid
+
+# PAR_BSTR_param_grid = {
+#     "aggreg_func": ['mean'],
+#     "size_sample": [0.7, 1.0],  # 0.7
+#     "n_estimators": [50],
+#     "depth": [7],  # 15
+#     "ens_metric_name": ["IBS"],  # ["bic", "roc", "ibs"], "IBS_WW", "IBS_REMAIN"
+#     "criterion": ["peto", "tarone-ware", "wilcoxon", "logrank"],  # "ibswei",
+#     "leaf_model": ["base_zero_after"],  # ["base_fast", "base_zero_after"]
+#     "balance": [None],  # [None, "balance", "balance+correct", "only_log_rank"]
+#
+#     "min_samples_leaf": [0.05, 0.01, 0.001],
+#     "max_features": [0.3, "sqrt"],
+#     "n_jobs": [15]
+# }
 
 # BOOST_param_grid = {
 #     "aggreg_func": ['wei', 'mean'],
@@ -47,7 +66,7 @@ BSTR_param_grid = {
 
 BOOST_param_grid = {
     "aggreg_func": ['mean'],  # ['wei', 'mean', 'median'],
-    "size_sample": [1.0],  # [0.5, 1.0] 0.7
+    "size_sample": [0.5, 1.0],  # [0.5, 1.0] 0.7
     "n_estimators": [50],
     "depth": [7],
     "l_reg": [0, 0.01, 0.1, 0.5, 0.9],
@@ -57,7 +76,7 @@ BOOST_param_grid = {
     "leaf_model": ["base_zero_after"],  # "base_zero_after", "base_fast"
     "balance": [None],  # [None, "balance", "balance+correct", "only_log_rank"]
 
-    "min_samples_leaf": [0.01],  # [0.01, 0.001]
+    "min_samples_leaf": [0.05, 0.01, 0.001],  # [0.01, 0.001]
     "max_features": [0.3, "sqrt"],
     "n_jobs": [5],
     "with_arc": [False],
@@ -106,6 +125,7 @@ CL_BOOST_param_grid = {
 SCHEME_PARAMS = {
     "TREE": CRAID_param_grid,
     "BSTR": BSTR_param_grid,
+    "PARBSTR": PAR_BSTR_param_grid,
     "BOOST": BOOST_param_grid,
     "CLEVERBOOST": CL_BOOST_param_grid
 }
--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -214,17 +214,17 @@ def run(dataset="GBSG", with_self=["TREE", "BSTR", "BOOST"],
     experim.set_metrics(lst_metrics)
     experim.add_metric_best(best_metric)
     if with_external:
-        # experim.add_method(CoxPHSurvivalAnalysis, cox_param_grid)
-        # experim.add_method(SurvivalTree, ST_param_grid)
-        # experim.add_method(RandomSurvivalForest, RSF_param_grid)
-        # experim.add_method(ComponentwiseGradientBoostingSurvivalAnalysis, CWGBSA_param_grid)
-        # experim.add_method(GradientBoostingSurvivalAnalysis, GBSA_param_grid)
-        # experim.add_method(LEAF_MODEL_DICT["base"], {})
-        # experim.add_method(LEAF_MODEL_DICT["baseLL"], {})
-        # experim.add_method(WeibullAFT, AFT_param_grid)
-        # experim.add_method(LogLogisticAFT, AFT_param_grid)
+        experim.add_method(CoxPHSurvivalAnalysis, cox_param_grid)
+        experim.add_method(SurvivalTree, ST_param_grid)
+        experim.add_method(RandomSurvivalForest, RSF_param_grid)
+        experim.add_method(ComponentwiseGradientBoostingSurvivalAnalysis, CWGBSA_param_grid)
+        experim.add_method(GradientBoostingSurvivalAnalysis, GBSA_param_grid)
+        experim.add_method(LEAF_MODEL_DICT["base"], {})
+        experim.add_method(LEAF_MODEL_DICT["baseLL"], {})
+        experim.add_method(WeibullAFT, AFT_param_grid)
+        experim.add_method(LogLogisticAFT, AFT_param_grid)
         experim.add_method(LogNormalAFT, AFT_param_grid)
-        # experim.add_method(CoxPH, CoxPH_param_grid)
+        experim.add_method(CoxPH, CoxPH_param_grid)
     if len(with_self) > 0:
         for alg in with_self:
             PARAMS_[dataset][alg]["categ"] = [categ]
@@ -257,7 +257,7 @@ def dir_path():
 @pytest.mark.parametrize(
     "dataset",  ["support2", "smarto"]  # "rott2", "PBC", "WUHAN", "GBSG", "flchain", "backblaze", "actg", "support2", "smarto"
 )
-def test_dataset_exp(dir_path, dataset, best_metric, bins_sch="origin", mode="CV"):  # CV+SAMPLE
+def test_dataset_exp(dir_path, dataset, best_metric, bins_sch="origin", mode="CV+SAMPLE"):  # CV+SAMPLE
     mode_wei = None
     # NORMAL_SHORT_QUANTILE_TIME_
     # prefix = f"{best_metric}_STRATTIME+_EXT10_NORMAL_EQ_REG_CLEVERBOOST_SUM_ALL_BINS_{bins_sch}"
@@ -267,20 +267,21 @@ def test_dataset_exp(dir_path, dataset, best_metric, bins_sch="origin", mode="CV
 
     # prefix = f"{best_metric}_STRATTIME+_EXT10_EQ_REG_TREE_ALL_BINS_{bins_sch}"
     # prefix = f"{best_metric}_STRATTIME+_EXT10_NORMAL_EQ_REG_TREE_ALL_BINS_{bins_sch}"
+    prefix = f"{best_metric}_STRATTIME+_EXT10_NORMAL_EQ_REG_PAR_BSTR_ALL_BINS_{bins_sch}"
     # prefix = f"{best_metric}_STRATTIME+_EXT10_NORMAL_EQ_REG_CLEVERBOOST_ALL_BINS_{bins_sch}"
     # prefix = f"{best_metric}_STRATTIME+_EXT10_NORMAL_EQ_REG_{mode_wei}_reg(0_01)_PART_BOOST_ALL_BINS_{bins_sch}"
 
-    prefix = f"{best_metric}_STRATTIME+_scsurv_test_LNAFT"  # "scsurv", "bstr_full_WB", SHORT_CNT_DIFF_
-    res_exp = run(dataset, with_self=[], with_external=True, mode=mode,
-                  # dir_path=dir_path+"\\",
-                  bins_sch=bins_sch, best_metric=best_metric)  # Only scikit-survival
+    # prefix = f"{best_metric}_STRATTIME+_scsurv_mean"  # "scsurv", "bstr_full_WB", SHORT_CNT_DIFF_
+    # res_exp = run(dataset, with_self=[], with_external=True, mode=mode,
+    #               # dir_path=dir_path+"\\",
+    #               bins_sch=bins_sch, best_metric=best_metric)  # Only scikit-survival
 
-    # storage_path = os.path.join("D:", os.sep, "Vasilev", "SA", dataset)
-    # if not os.path.exists(storage_path):
-    #     os.makedirs(storage_path)
-    # res_exp = run(dataset, with_self=["PARBSTR"], with_external=False, mode=mode,  # CLEVERBOOST
-    #               # dir_path=storage_path+"\\",
-    #               bins_sch=bins_sch, best_metric=best_metric, mode_wei=mode_wei)  # ["TREE", "BSTR", "BOOST"]
+    storage_path = os.path.join("D:", os.sep, "Vasilev", "SA", dataset)
+    if not os.path.exists(storage_path):
+        os.makedirs(storage_path)
+    res_exp = run(dataset, with_self=["PARBSTR"], with_external=False, mode=mode,  # CLEVERBOOST
+                  # dir_path=storage_path+"\\",
+                  bins_sch=bins_sch, best_metric=best_metric, mode_wei=mode_wei)  # ["TREE", "BSTR", "BOOST"]
 
     df_full = res_exp.get_result()
     df_criterion = res_exp.get_best_by_mode(stratify="criterion")
