5c46b32e0cbedf228af91244068d633c81d94551
vasiliev <yuliivasiliev@gmail.com>
2024-05-08 17:14:14 +0300

    Final try to fix sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ autosummary_generate = True
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"  # 'alabaster'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 html_context = {
