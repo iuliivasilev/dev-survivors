1d6dae8e66a339d8ddf5acdc0306818799f620c1
vasiliev <yuliivasiliev@gmail.com>
2024-05-08 15:55:39 +0300

    Try to fix static.yml to run github pages

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,9 @@ autosummary_generate = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"  # 'alabaster'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 sphinx_gallery_conf = {
