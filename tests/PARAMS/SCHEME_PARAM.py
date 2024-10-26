50c5da0d5a872fd9d8650b8c99cebf2cfa07ad9f
vasiliev <yuliivasiliev@gmail.com>
2024-10-26 23:48:33 +0300

    Updated hyperparams

--- tests/PARAMS/SCHEME_PARAM.py
+++ tests/PARAMS/SCHEME_PARAM.py
@@ -11,19 +11,53 @@
 #     "max_features": [1.0]
 # }
 
+# BACKBLAZE
+# CRAID_param_grid = {
+#     "depth": [0, 1, 2, 5, 7, 10],  # 10
+#     "balance": [None],
+#     "criterion": ["logrank"],  # ["peto", "tarone-ware", "wilcoxon", "logrank"],
+#     "min_samples_leaf": [0.3, 0.1, 0.05],
+#     "l_reg": [0],
+#     "leaf_model": ["base", "WeibullAFT", "LogNormalAFT", "LogLogisticAFT", "CoxPH"],
+#     'cut': [False],  # True,
+#     "woe": [True],
+#     "signif": [0.05],
+#     "max_features": [1.0],
+#     "leaf_penalizer": [0, 0.1, 0.5, 1.0]
+# }
+
+# BEST HYPERPARAMETERS
 CRAID_param_grid = {
-    "depth": [0, 1, 2, 5, 7, 10],  # 10
-    "balance": [None],
-    "criterion": ["logrank"],  # ["peto", "tarone-ware", "wilcoxon", "logrank"],
-    "min_samples_leaf": [0.3, 0.1, 0.05, 0.01, 0.001],
-    "l_reg": [0],
-    "leaf_model": ["base", "WeibullAFT", "LogNormalAFT", "LogLogisticAFT", "CoxPH"],
-    'cut': [False],  # True,
-    "woe": [True],
-    "signif": [0.05],
-    "max_features": [1.0]
+    'balance': [None],
+    'criterion': ['wilcoxon', 'logrank'],
+    'cut': [False],
+    'depth': [10],
+    'ens_metric_name': ['IBS_REMAIN'],
+    'l_reg': [0.1, 0.9, 0.01, 0],
+    'leaf_model': ['base_zero_after'],  # , 'base'
+    'max_features': [1.0],
+    'min_samples_leaf': [0.05],
+    'mode_wei': [None],
+    'signif': [0.05, 1.0, 0.1],
+    'woe': [True]
 }
 
+# {
+#     'balance': [None],
+#     'criterion': ['wilcoxon', 'tarone-ware', 'logrank'],
+#     'cut': [False],
+#     'depth': [15],
+#     'ens_metric_name': ['IBS_REMAIN'],
+#     'l_reg': [0.9, 0, 0.1, 0.01],
+#     'leaf_model': ['base_zero_after'],
+#     'max_features': [1.0],
+#     'min_samples_leaf': [0.05, 0.01],
+#     'n_jobs': [10],
+#     'signif': [0.1, 1.0, 0.05],
+#     'woe': [True]
+# }
+
+# BACKBLAZE
 BSTR_param_grid = {
     "aggreg_func": ["mean"],
     "size_sample": [0.3, 0.5, 0.7, 1.0],  # [0.3, 0.5, 0.7, 1.0]
@@ -41,6 +75,23 @@ BSTR_param_grid = {
 }
 
 PAR_BSTR_param_grid = BSTR_param_grid
+
+# BEST HYPERPARAMETERS
+PAR_BSTR_param_grid = {
+    'aggreg_func': ['mean'],
+    'balance': [None],
+    'criterion': ['wilcoxon', 'peto', 'logrank'],
+    'depth': [7],
+    'ens_metric_name': ['IBS_REMAIN'],
+    'l_reg': [0, 0.01],
+    'leaf_model': ['base_zero_after'],
+    'max_features': ['sqrt'],
+    'min_samples_leaf': [0.05, 0.01],
+    'mode_wei': [None],
+    'n_estimators': [50],
+    'n_jobs': [10],
+    'size_sample': [0.3, 0.7, 1.0, 0.5]
+}
 
 # PAR_BSTR_param_grid = {
 #     "aggreg_func": ['mean'],
@@ -76,23 +127,43 @@ PAR_BSTR_param_grid = BSTR_param_grid
 #     "all_weight": [False, True]
 # }
 
-BOOST_param_grid = {
-    "aggreg_func": ['mean'],  # ['wei', 'mean', 'median'],
-    "size_sample": [0.5, 1.0],  # [0.5, 1.0] 0.7
-    "n_estimators": [50],
-    "depth": [7],
-    "l_reg": [0, 0.01, 0.1, 0.5, 0.9],
-    "mode_wei": ['linear'],  # 'linear', 'exp', 'square', "sigmoid", "softmax"],
-    "ens_metric_name": ["IBS_REMAIN"],  # ["bic", "roc", "ibs", "IBS_WW", "IBS_REMAIN"
-    "criterion": ["peto", "tarone-ware", "wilcoxon", "logrank"],
-    "leaf_model": ["base_zero_after"],  # "base_zero_after", "base"
-    "balance": [None],  # [None, "balance", "balance+correct", "only_log_rank"]
+# BACKBLAZE
+# BOOST_param_grid = {
+#     "aggreg_func": ['mean'],  # ['wei', 'mean', 'median'],
+#     "size_sample": [0.5, 1.0],  # [0.5, 1.0] 0.7
+#     "n_estimators": [50],
+#     "depth": [7],
+#     "l_reg": [0, 0.01, 0.1, 0.5, 0.9],
+#     "mode_wei": ['linear'],  # 'linear', 'exp', 'square', "sigmoid", "softmax"],
+#     "ens_metric_name": ["IBS_REMAIN"],  # ["bic", "roc", "ibs", "IBS_WW", "IBS_REMAIN"
+#     "criterion": ["peto", "tarone-ware", "wilcoxon", "logrank"],
+#     "leaf_model": ["base_zero_after"],  # "base_zero_after", "base"
+#     "balance": [None],  # [None, "balance", "balance+correct", "only_log_rank"]
+#
+#     "min_samples_leaf": [0.05, 0.01, 0.001],  # [0.01, 0.001]
+#     "max_features": [0.3, "sqrt"],
+#     "with_arc": [False],
+#     'weighted_tree': [False],
+#     "all_weight": [False]  # [False, True]
+# }
 
-    "min_samples_leaf": [0.05, 0.01, 0.001],  # [0.01, 0.001]
-    "max_features": [0.3, "sqrt"],
-    "with_arc": [False],
+# BEST HYPERPARAMETERS
+BOOST_param_grid = {
+    'aggreg_func': ['mean'],
+    'all_weight': [False],
+    'balance': [None],
+    'criterion': ['wilcoxon', 'peto', 'tarone-ware', 'logrank'],
+    'depth': [7],
+    'ens_metric_name': ['IBS_REMAIN'],
+    'l_reg': [0, 0.01],
+    'leaf_model': ['base_zero_after'],
+    'max_features': [0.3, 'sqrt'],
+    'min_samples_leaf': [0.05, 0.01],
+    'mode_wei': ['linear'],
+    'n_estimators': [50],
+    'size_sample': [0.5, 1.0],
     'weighted_tree': [False],
-    "all_weight": [False]  # [False, True]
+    'with_arc': [False]
 }
 
 CL_BOOST_param_grid = {
