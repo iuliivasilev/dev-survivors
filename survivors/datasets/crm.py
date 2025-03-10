77e3d610f3f5a8ac92c09716429a621ba9d74b33
vasiliev <yuliivasiliev@gmail.com>
2025-03-10 10:59:00 +0300

    Added new template for CRM datasets

---
+++ survivors/datasets/crm.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import re
+import random
+from os.path import dirname, join
+from ..constants import TIME_NAME, CENS_NAME, get_y
+
+random.seed(10)
+
+
+def load_ecomm_dataset():
+    """
+    https://www.kaggle.com/datasets/ankitverma2010/ecommerce-customer-churn-analysis-and-prediction
+    """
+    pass
+    return None
+
+
+def load_telco_dataset():
+    """
+    https://www.kaggle.com/code/bhartiprasad17/customer-churn-prediction/data
+    https://www.interviewquery.com/p/customer-churn-datasets
+    https://github.com/Pradnya1208/Telecom-Customer-Churn-prediction
+    https://github.com/archd3sai/Customer-Survival-Analysis-and-Churn-Prediction/blob/master/Customers%20Survival%20Analysis.ipynb
+    """
+    pass
+    return None
+
+
+def load_bank_dataset():
+    """
+    https://www.kaggle.com/datasets/shrutimechlearn/churn-modelling
+    """
+    pass
+    return None
